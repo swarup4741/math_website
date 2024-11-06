dfe2210cfaf58d65d9d091a29cd7f06b2bbc188f
Swarup <313swarup@gmail.com>
2024-11-06 20:16:37 +0530

    resizing of margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="px-1 md:10px md:py-5">
       <div className="flex flex-col lg:flex-row lg:justify-around lg:align-middle">
-      <div className="mr-10">
+      <div className="lg:mr-10">
       <h1 className="subpixel-antialiased text-3xl md:text-4xl lg:text-5xl mb-10 mt-5">Swarup Kumar Das</h1>
       <p className="subpixel-antialiased md:text-lg lg:text-lg mb-5">I am a Ph.D. student at the <Link className="text-blue-500" href="https://www.tcgcrest.org/researches/translational-research-in-mathematics-trim/" target="_blank">TRIM (Translation Research in Mathematics)</Link> vertical of <Link className="text-blue-500" href="https://www.tcgcrest.org/institutes/iai/" target="_blank">Institute for Advancing Intelligence, TCG CREST</Link>. I am currently pursuing my research in the area of low-dimensional topology under the supervision of <Link href="https://www.tcgcrest.org/people/apratim-chakraborty/" target="_blank" className="text-blue-500">Dr. Apratim Chakraborty</Link>.</p>
       <p className="subpixel-antialiased md:text-lg lg:text-lg mb-5">My research interest lies within the study of invariants like Khovanov homology, Heegard Floer homology and other invariants coming from these theories to study knots/links, 3-manifolds, 4-manifolds and interactions between them.</p>
