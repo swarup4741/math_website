98e8c17525964d0f47d11daffc636ba79aec0adc
Swarup <313swarup@gmail.com>
2024-11-08 01:48:26 +0530

    corrected mail address

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
        <h1 className="subpixel-antialiased text-xl font-bold md:text-2xl lg:text-3xl mb-10 mt-10">Contact</h1>
        <Image className="mb-10" src="/contact.webp" height="300" width="300" alt="Popeye making a water knot"/>
 
-       <p className="mb-7"><strong>email - </strong> swarup.math@gmail.com / swarup.das.83@tcgcrest.org</p>
+       <p className="mb-7"><strong>email - </strong> swarupdas.math@gmail.com / swarup.das.83@tcgcrest.org</p>
        <p><strong>Office - </strong>First Floor, Tower 1, Bengal Eco Intelligent Park (Techna), Block EM, Plot No 3, Sector V, Salt lake, Kolkata 700091, West Bengal, INDIA</p>
     </div>
   );
